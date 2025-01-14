3810edfe6868434d386f731daf4224de0d9a8eff
lixiwen621 <592864759@qq.com>
2025-01-14 16:28:43 +0800

    Commit computeFactorial

---
+++ src/main/java/com/arithmetic/math/Factorial.java
@@ -0,0 +1,28 @@
+package com.arithmetic.math;
+
+/**
+ *  阶乘的算法
+ *  5! = 1 * 2 * 3 * 4 * 5
+ */
+public class Factorial {
+
+    private static int prod(int a, int b){
+        return a * b;
+    }
+
+    private static int computeFactorial(int num){
+        int i = 1;
+        int n = i;
+        while (num >= 1){
+            n = prod(i,n);
+            i += 1;
+            num --;
+        }
+        return n;
+    }
+
+    public static void main(String[] args) {
+        int factorial = computeFactorial(4);
+        System.out.println(factorial);
+    }
+}
